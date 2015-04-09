59b7139403cbde362026d33237fa4cc7a814e524
successivesoftware <siddharth@successivesoftwares.com>
2015-04-09 11:30:45 +0530

    Small Changes

--- PauseSystem/PauseSystem/Controllers/HomeController.cs
+++ PauseSystem/PauseSystem/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PauseSystem.Models;
+using PauseSystem.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,11 +11,25 @@ namespace PauseSystem.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+        UnitOfWork unitOfWork = new UnitOfWork();
+        Repository<Levering> leveringRepository;
+
+        public HomeController()
+        {
+            this.leveringRepository = unitOfWork.Repository<Levering>();
+        }
 
         [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult GetLeverings()
+        {
+            var items = leveringRepository.Table.Take(10).ToList();
+            return PartialView("_UCLiverings", items);
         }
 
         [Authorize(Roles=UserRoleTypes.Customer)]
