ba756f05b073eb5972a85c0a5aceeeaef47ba9c0
riley2200 <bd@monsterservice.dk>
2015-07-14 10:43:16 +0200

    Added Req product

---
+++ PauseSystem/PauseSystem/Models/Entity/RequiredProduktRelation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SubusiDbEf.BaseClass;
+
+namespace SubusiDbEf.Models
+{
+    [Serializable]
+    public class RequiredProduktRelation : BindableObject
+    {
+        private int _id, _ratio;
+        private Produkt _fromProdukt, _toProdukt;
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                RaisePropertyChanging(() => Id);
+                _id = value;
+                RaisePropertyChanged(() => Id);
+            }
+        }
+
+        public int FromProduktId { get; set; }
+        public int ToProduktId { get; set; }
+
+        public int Ratio
+        {
+            get { return _ratio; }
+            set
+            {
+                RaisePropertyChanging(() => Ratio);
+                _ratio = value;
+                RaisePropertyChanged(() => Ratio);
+            }
+        }
+
+        public virtual Produkt FromProdukt
+        {
+            get
+            {
+                return _fromProdukt;
+
+            }
+            set
+            {
+                RaisePropertyChanging(() => FromProdukt);
+                _fromProdukt = value;
+                RaisePropertyChanged(() => FromProdukt);
+            }
+        }
+
+        public virtual Produkt ToProdukt
+        {
+            get { return _toProdukt; }
+            set
+            {
+                RaisePropertyChanging(()=>ToProdukt);
+                _toProdukt = value;
+                RaisePropertyChanged(()=>ToProdukt);
+            }
+        }
+
+    }
+}
---
+++ PauseSystem/PauseSystem/Models/Mapping/RequiredProduktRelationMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubusiDbEf.Models.Mappings
+{
+    public class RequiredProduktRelationMap : EntityTypeConfiguration<RequiredProduktRelation>
+    {
+        public  RequiredProduktRelationMap() : base()
+        {
+            HasKey(p => p.Id);
+            Property(p => p.Id).HasColumnName("Id").HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(p => p.FromProduktId).HasColumnName("FromProduktId");
+            Property(p => p.ToProduktId).HasColumnName("ToProduktId");
+            Property(p => p.Ratio).HasColumnName("Ratio");
+            HasRequired(p => p.FromProdukt).WithMany(p=>p.RequiredProduktRelations).HasForeignKey(p => p.FromProduktId);
+            HasRequired(p => p.ToProdukt).WithMany(p=>p.ProduktRequiredRelations).HasForeignKey(p => p.ToProduktId);
+
+
+            ToTable("RequiredProduktRelation");
+
+        }
+    }
+}
